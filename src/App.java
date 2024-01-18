7ead33a677760d84de89a96945389848ae9037d7
Laboratorio 109 Pc-12 <lab-109-inf-12>
2024-01-17 21:31:15 -0400

    primer commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class App {
+public class App {
+    public static void main(String[] args) {
+        String nombre;
+        double monto;
+        double resultado;
+
+        nombre = "Roger";
+        monto = 475.50;
+        resultado = monto * 1.15;
+
+        System.out.println("Saludos " + nombre + ", el monto total de su transacción es de" + resultado);
+
+    }
 }
